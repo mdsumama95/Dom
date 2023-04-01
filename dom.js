9568c4407507f7ea1debce8ee40e6c709c5a552a
mdsumama95 <62618894+mdsumama95@users.noreply.github.com>
2023-04-01 17:56:00 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,8 @@
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+var items = document.getElementById('items');
+console.log(items);
+items.style.Color='green';
+items.style.fontWeight = 'bold';
+items.style.backgroundColor = '#f4f4f4';
