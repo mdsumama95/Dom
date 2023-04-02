c832f4d85d49595f34955154742d095963bc45fb
mdsumama95 <62618894+mdsumama95@users.noreply.github.com>
2023-04-02 14:30:06 +0530

    queryselector

--- dom.js
+++ dom.js
@@ -1,14 +1,9 @@
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
 
-var li = document.getElementsByTagName('li');
-console.log(items);
-console.log(items[1]);
-li[1].textContent = 'Hello 2'
-li[0].style.fontWeight = 'bold';
-li[1].style.fontWeight = 'bold';
-li[2].style.fontWeight = 'bold';
-li[3].style.fontWeight = 'bold';
-for(var i = 0; i <li.length; i++){
-    li[i].style.backgroundColor ='#f4f4f4';
-}
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.backgroundColor = '#3a8000'
+
+var VisItem = document.querySelector('.list-group-item:nth-child(3)');
+VisItem.style.display = "none";
