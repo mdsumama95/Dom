7c8d22367278a3f9f93efe5d02a9eddc0d4f559a
mdsumama95 <62618894+mdsumama95@users.noreply.github.com>
2023-04-02 12:44:50 +0530

    add list item

--- dom.js
+++ dom.js
@@ -1,13 +1,14 @@
 var header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px #000';
 
-var items = document.getElementsByClassName('list-group-item');
+var li = document.getElementsByTagName('li');
 console.log(items);
 console.log(items[1]);
-items[1].textContent = 'Hello 2'
-items[0].style.fontWeight = 'bold';
-items[1].style.fontWeight = 'bold';
-items[2].style.fontWeight = 'bold';
-items[3].style.fontWeight = 'bold';
-items[1].style.backgroundColor = '#f4f4f4';
-items[2].style.backgroundColor = '#008000';
+li[1].textContent = 'Hello 2'
+li[0].style.fontWeight = 'bold';
+li[1].style.fontWeight = 'bold';
+li[2].style.fontWeight = 'bold';
+li[3].style.fontWeight = 'bold';
+for(var i = 0; i <li.length; i++){
+    li[i].style.backgroundColor ='#f4f4f4';
+}
